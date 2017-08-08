9eeb67b83df27b1b7becd081e499d6e799aacd13
sudocoder <nikhilt231@gmail.com>
2017-08-08 13:26:00 +0530

    final keyword

---
+++ src/tripathy/nikhil/JVM/FinalKeyWord.java
@@ -0,0 +1,11 @@
+package tripathy.nikhil.JVM;
+
+public class FinalKeyWord {
+final static int i;
+static {
+    i=9;
+}
+public static void main(String...strings ) {
+    System.out.println(i);
+}
+}
