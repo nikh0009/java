454737a6d4602f40ca91fbf1b3a5a97e61891bc7
sudocoder <nikhilt231@gmail.com>
2017-08-12 18:35:52 +0530

    Improved version.Taking path from the users.

--- src/tripathy/nikhil/IO/File/Maker.java
+++ src/tripathy/nikhil/IO/File/Maker.java
@@ -2,16 +2,32 @@
 package tripathy.nikhil.IO.File;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner; 
 public class Maker {
 public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the path to create directory");
+    String path = in.nextLine();
+    String filename="";
    try {
-       File f =new File("C:\\Users\\ntripath\\Desktop\\nikhiltr");
-       f.mkdir();
-       File file = new File(f,"firstFile.txt");
-       file.createNewFile();
-       System.out.println(file.getFreeSpace());
+       File f =new File(path);
+       //Creates a directory in the given file path.Returns boolean value.
+       if(f.mkdir()) {
+	   System.out.println("Directory created!");
+       }else
+	   System.out.println("Directory Exists!");
+       System.out.println("Enter the file name to be created!");
+       filename = in.nextLine();
+       File file = new File(f,filename);
+       //Creates the file in the specified directory.Returns boolean value.
+       if(file.createNewFile())
+       System.out.println("File created!");
+       else
+	   System.out.println("File exists!");
    }catch(IOException e){
        e.printStackTrace();
+   }finally {
+       in.close();
    }
 }
 }
--- src/tripathy/nikhil/IO/File/RemoveFile.java
+++ src/tripathy/nikhil/IO/File/RemoveFile.java
@@ -1,17 +1,34 @@
 //Java program to delete an existing file.
 package tripathy.nikhil.IO.File;
 import java.io.File;
+import java.util.Scanner;
 public class RemoveFile {
 public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the path of directory or file to be delete");
+    String path = in.nextLine();
     try {
-	 File f1 = new File("C:\\Users\\ntripath\\Desktop\\nikhiltr\\firstFile.txt");
+	 File f1 = new File(path);
 	 if(f1.delete()) {
-		   System.out.println("file deleted");
+		   System.out.println("Deleted!");
 	       }
-	       else
-		   System.out.println("Not deleted");
+	       else {
+		   if(f1.isDirectory()) {
+		       File[] f=f1.listFiles();
+		       for(File ff:f)
+			   System.out.println(ff.getName());
+		       System.out.println("Its a directory. Make sure its empty. Can't delete.");
+		   }
+		       
+		   else
+		       System.out.println("Incorrct path or file does'nt exists try again!");
+	       }
+		   
     }catch(Exception e) {
 	e.printStackTrace();
+    }
+    finally{
+	in.close();
     }
 }
 }
